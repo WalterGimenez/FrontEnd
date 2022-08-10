87981d07b83bb661252e67821621bbe76581742b
WalterGimenez <wallegimenez@gmail.com>
2022-08-10 18:38:20 -0300

    se crearon los componentes about,button,education,experience,footer,header,error,proyect y skill

--- gimenezApp/src/app/app.module.ts
+++ gimenezApp/src/app/app.module.ts
@@ -4,11 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ButtonComponent } from './components/button/button.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ButtonComponent,
+    EducationComponent,
+    ExperienceComponent,
+    FooterComponent,
+    HeaderComponent,
+    ErrorComponent,
+    ProyectComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
