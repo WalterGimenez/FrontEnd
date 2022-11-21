8a85c0f45a7aa16e127d97a85d001017a420083d
WalterGimenez <wallegimenez@gmail.com>
2022-11-21 14:54:04 -0300

    se agregó modal de Login y se mejoró la app de button

--- gimenezApp/src/app/components/button/button.component.ts
+++ gimenezApp/src/app/components/button/button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() text!: string;
   @Input() id!: string;
+  @Input() class!: string;
 
   constructor() { }
 
