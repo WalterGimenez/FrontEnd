6796ff2f18a2b873b124d4405c0101735d6ea880
WalterGimenez <wallegimenez@gmail.com>
2022-12-16 14:44:27 -0300

    se modificó para que traiga los datos de Educación desde la base de datos

--- gimenezApp/src/app/components/education/education.component.ts
+++ gimenezApp/src/app/components/education/education.component.ts
@@ -1,5 +1,7 @@
+import { education } from './../../model/education.model';
 import { Component, OnInit } from '@angular/core';
 import { LoggedService } from 'src/app/service/logged.service';
+import { EducationService } from 'src/app/service/education.service';
 
 @Component({
   selector: 'app-education',
@@ -8,13 +10,20 @@ import { LoggedService } from 'src/app/service/logged.service';
 })
 export class EducationComponent implements OnInit {
   isLoggedIn = false;
+  education : education [] = []; 
 
-  constructor(private logginService : LoggedService) { }
+  constructor(private logginService : LoggedService, private serviceEducation : EducationService) { }
 
   ngOnInit(): void {
-   
+    this.loadEducation();
     this.logginService.login$.subscribe((loginState => {
     this.isLoggedIn = loginState;}))
+  }
+
+  loadEducation(): void{
+    this.serviceEducation.list().subscribe(
+      data => { this.education = data; }
+    )
   }
 
 }
