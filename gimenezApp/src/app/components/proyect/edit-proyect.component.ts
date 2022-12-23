12d78869bde9056003acdda6edf0e5bb6706041d
WalterGimenez <wallegimenez@gmail.com>
2022-12-23 16:12:33 -0300

    se agregó EditProyect para modificar Proyecto en la base de datos

--- gimenezApp/src/app/components/proyect/edit-proyect.component.ts
+++ gimenezApp/src/app/components/proyect/edit-proyect.component.ts
@@ -1,4 +1,7 @@
+import { proyect } from './../../model/proyect.model';
 import { Component, OnInit } from '@angular/core';
+import { ProyectService } from 'src/app/service/proyect.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-proyect',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-proyect.component.css']
 })
 export class EditProyectComponent implements OnInit {
+  proyect : proyect = null;
 
-  constructor() { }
+  constructor(private proyectService : ProyectService,private activatedRouter : ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRouter.params.subscribe(params => {
+      const id = params['id'];
+      this.proyectService.detail(id).subscribe(
+        data =>{
+          this.proyect = data;
+        }, err =>{
+          alert("Error al pasar el id");
+          this.router.navigate(['']);
+        }
+      )
+    });
+
+  }
+
+
+  update(): void{
+    this.activatedRouter.params.subscribe(params => {
+      const id = params['id'];
+      this.proyectService.update(id, this.proyect).subscribe(
+        data => {
+      
+          this.router.navigate(['']);
+        }, err => {
+          alert("Error al querer modificar la educacion");
+          this.router.navigate(['']);
+        }
+      )
+    });
+  }
+
+  canceled(){
+    alert("No se realizaron cambios");
+    this.router.navigate(['']);
   }
 
 }
